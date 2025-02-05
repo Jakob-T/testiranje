70c43d260d71db767111cf52440a30ae8674f62e
Jakob-T <trelec031@gmail.com>
2025-02-05 17:33:54 +0100

    update error

--- test_demoblaze.py
+++ test_demoblaze.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 def setup():
@@ -12,14 +14,14 @@ def setup():
     driver.maximize_window()
     return driver
 
-def test_login():
-    driver = setup()
+def test_login(driver):
     driver.get("https://www.demoblaze.com/")
     driver.implicitly_wait(10)
 
     login_button = driver.find_element(By.ID, "login2")
     login_button.click()
-    time.sleep(2)
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "loginusername")))
 
     username = driver.find_element(By.ID, "loginusername")
     username.send_keys("testuserjakob")
@@ -30,52 +32,37 @@ def test_login():
     submit = driver.find_element(By.XPATH, "//button[text()='Log in']")
     submit.click()
 
-    time.sleep(3)
-
-    welcome_text = driver.find_element(By.ID, "nameofuser")
+    welcome_text = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "nameofuser")))
     assert "Welcome" in welcome_text.text
 
     print("Login test passed!")
-    driver.quit()
 
-def test_logout():
-    driver = setup()
-    driver.get("https://www.demoblaze.com/")
-    driver.implicitly_wait(10)
-
-    test_login()
-
-    logout_button = driver.find_element(By.ID, "logout2")
+def test_logout(driver):
+    logout_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "logout2")))
     logout_button.click()
-    time.sleep(3)
 
-    assert driver.find_element(By.ID, "login2").is_displayed()
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "login2")))
+
     print("Logout test passed!")
-    driver.quit()
 
-def test_add_to_favorites():
-    driver = setup()
-    driver.get("https://www.demoblaze.com/")
-    driver.implicitly_wait(10)
+def test_add_to_favorites(driver):
+    driver.get("https://www.demoblaze.com/prod.html?idp_=1")  # Otvaranje proizvoda
 
-    test_login()
-
-    driver.get("https://www.demoblaze.com/prod.html?idp_=1")#ovo je za otvaranje nekoog proizvoda
-    time.sleep(2)
-
-    fav_button = driver.find_element(By.XPATH, "//a[text()='Add to favorite']")
+    fav_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[text()='Add to favorite']")))
     fav_button.click()
 
-    time.sleep(2)
-
+    WebDriverWait(driver, 5).until(EC.alert_is_present())
     alert = driver.switch_to.alert
     assert "Added" in alert.text
     alert.accept()
 
     print("Add to favorites test passed!")
-    driver.quit()
 
 if __name__ == "__main__":
-    test_login()
-    test_logout()
-    test_add_to_favorites()
+    driver = setup()
+    try:
+        test_login(driver)
+        test_add_to_favorites(driver)
+        test_logout(driver)
+    finally:
+        driver.quit()
